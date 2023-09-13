50acb1253f488a90c857968723e2af1e45e8bbfa
unknown <vojasvi04@gmail.com>
2023-09-13 17:51:10 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import "./App.css";
 import Card from "./Component/Card/Card";
 import Filter from "./Component/Filter/Filter";
 import Footer from "./Component/Footer/Footer";
-import Photo from "./Component/Image/Photo";
+// import Photo from "./Component/Image/Photo";
 import Navbar from "./Component/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Photo />
+      {/* <Photo /> */}
       <Filter />
       <Card />
       <Footer />
--- src/Component/Image/Photo.jsx
+++ src/Component/Image/Photo.jsx
@@ -1,42 +1,42 @@
-import React from "react";
-// import "./Photo.modules.css";
-const Photo = () => {
-  return (
-    <>
-      <div>
-        <h1 style={{ marginLeft: " 18%" }}>Trending Posts</h1>
-      </div>
-      <div style={{ marginTop: "-1%" }}>
-        <img
-          style={{ marginLeft: "18%", marginTop: "3%", borderRadius: "4px" }}
-          src="https://edyoda.s3.amazonaws.com/media/blog-images/ace-soft-skills-small.jpg"
-          alt=""
-        />
-        <img
-          style={{
-            marginBottom: "25px",
-            marginLeft: "1.5%",
-            marginTop: "215px",
-            borderRadius: " 4px",
-          }}
-          src="https://edyoda.s3.amazonaws.com/media/blog-images/learn_cloud_computing.jpeg"
-          alt=""
-          width="300px"
-        />{" "}
-        <img
-          style={{
-            marginBottom: "231px",
-            marginLeft: "65%",
-            marginTop: "-645px",
-            borderRadius: "4px",
-          }}
-          src="https://edyoda.s3.amazonaws.com/media/blog-images/aptitude-small.jpg"
-          alt=""
-          width="300px"
-        />
-      </div>
-    </>
-  );
-};
+// import React from "react";
+// // import "./Photo.modules.css";
+// const Photo = () => {
+//   return (
+//     <>
+//       <div>
+//         <h1 style={{ marginLeft: " 18%" }}>Trending Posts</h1>
+//       </div>
+//       <div style={{ marginTop: "-1%" }}>
+//         <img
+//           style={{ marginLeft: "18%", marginTop: "3%", borderRadius: "4px" }}
+//           src="https://edyoda.s3.amazonaws.com/media/blog-images/ace-soft-skills-small.jpg"
+//           alt=""
+//         />
+//         <img
+//           style={{
+//             marginBottom: "25px",
+//             marginLeft: "1.5%",
+//             marginTop: "215px",
+//             borderRadius: " 4px",
+//           }}
+//           src="https://edyoda.s3.amazonaws.com/media/blog-images/learn_cloud_computing.jpeg"
+//           alt=""
+//           width="300px"
+//         />{" "}
+//         <img
+//           style={{
+//             marginBottom: "231px",
+//             marginLeft: "65%",
+//             marginTop: "-645px",
+//             borderRadius: "4px",
+//           }}
+//           src="https://edyoda.s3.amazonaws.com/media/blog-images/aptitude-small.jpg"
+//           alt=""
+//           width="300px"
+//         />
+//       </div>
+//     </>
+//   );
+// };
 
-export default Photo;
+// export default Photo;
